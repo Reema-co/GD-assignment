dd49a6e709a8ffb016bb7d0a3befee2b84122276
Reema-co <158995415+Reema-co@users.noreply.github.com>
2024-02-12 12:19:20 +0300

    Week1.Day2

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Vector3 forwardMove = new Vector3(0.2f, 0, 0);
+    Vector3 BackourdMove = new Vector3(-0.1f, 0, 0);
+    Vector3 RightMove = new Vector3(0, 0, -0.1f);
+    Vector3 leftMove = new Vector3(0, 0, 0.1f);
+
+     void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += forwardMove;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += BackourdMove;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += RightMove;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += leftMove;
+        }
+
+    }
+}
